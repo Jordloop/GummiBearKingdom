82c2fec78d3c11707c9fc5123eca2834c2eacd8a
JordLoop <Jordloop@gmail.com>
2017-08-04 09:07:24 -0700

    Add model for Gummi and Recview, add DB dependencies to project.json

---
+++ GummiBearKingdom/src/GummiBearKingdom/Models/Gummi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GummiBearKingdom.Models
+{
+    public class Gummi
+    {
+        [Key]
+        public int GummiId { get; set; }
+        public string Name { get; set; }
+        public string Origin { get; set; }
+        public string Flavor { get; set; }
+        public int Cost { get; set; }
+        public virtual ICollection<Review> Reviews{ get; set; }
+    }
+}
