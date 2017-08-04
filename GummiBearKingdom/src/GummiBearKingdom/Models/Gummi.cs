2b01bb45bdb2a7f412d90f10e37511129e0b0671
JordLoop <Jordloop@gmail.com>
2017-08-04 13:23:43 -0700

    Recreate db migration. Failed: Same issue as before-- No executable found matching command 'dotnet-ef'. Going to try one more time

---
+++ GummiBearKingdom/src/GummiBearKingdom/Models/Gummi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GummiBearKingdom.Models
+{
+    public class Gummi
+    {
+        [Key]
+        public int GummiId { get; set; }
+        public string Name { get; set; }
+        public string Origin { get; set; }
+        public string Cost { get; set; }
+    }
+}
